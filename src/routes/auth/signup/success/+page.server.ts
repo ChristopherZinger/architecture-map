ca84f3979cd4d0443aca5e905195be8d3a7ac53b
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-05-27 19:34:02 +0200

    remove success page

--- src/routes/auth/signup/success/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export const load = ({ locals }) => {
-	if (!locals.user) {
-		throw error(401, 'Unauthorized');
-	}
-};
--- src/routes/auth/signup/success/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Success</h1>
-<p>Your account has been created</p>
