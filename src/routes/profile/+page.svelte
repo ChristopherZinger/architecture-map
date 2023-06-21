85dc6a304f89939b6a6d484a4f2116b8b3802aa4
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-21 22:30:01 +0200

    style profil page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,11 +2,14 @@
 	import { appUser } from '$lib/stores/appUser';
 </script>
 
-<h1>User Profile</h1>
-{#if $appUser}
-	<ul>
-		{#if $appUser.name} <li>user: ${$appUser.name}</li>{/if}
-		<li>email: {$appUser.email}</li>
-		<li>role: {$appUser.role}</li>
-	</ul>
-{/if}
+<div class="container">
+	<div class="mt-16 flex flex-col gap-5">
+		<h1 class="text-xl font-bold">User Profile</h1>
+		{#if $appUser}
+			<ul>
+				{#if $appUser.name} <li>user: ${$appUser.name}</li>{/if}
+				<li>Email: <b>{$appUser.email}</b></li>
+			</ul>
+		{/if}
+	</div>
+</div>
