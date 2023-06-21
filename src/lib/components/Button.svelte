6d832be333667575fee8f3940f2eac191d9ef027
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-21 21:31:16 +0200

    add styled button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let type: 'button' | 'submit' | null | 'reset' = 'submit';
+</script>
+
+<button class="border-2 border-black h-10 hover:text-white hover:bg-black font-bold px-5" {type}
+	><slot /></button
+>
