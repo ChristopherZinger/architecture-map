05528921a7c90ae107c95a38578fed7e17698e48
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-21 20:49:32 +0200

    top nav skeleton

---
+++ src/lib/components/navs/TopNav.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { appUser } from '$lib/stores/appUser';
+	import Fa from 'svelte-fa';
+	import {
+		faUser,
+		// faSliders,
+		// faMagnifyingGlass,
+		faArrowRightFromBracket
+	} from '@fortawesome/free-solid-svg-icons';
+
+	async function logout() {
+		await fetch('/api/logout', {
+			method: 'POST'
+		});
+		appUser.setUser(null);
+	}
+</script>
+
+<div class="h-20 flex items-center mx-10">
+	<div class="flex justify-between grow">
+		<ul class="flex gap-5">
+			<li><a href="/" class="font-bold">Modernism Map</a></li>
+		</ul>
+		<ul class="flex items-center gap-5">
+			<!-- <li class="w-4 h-4 flex items-center"><Fa icon={faMagnifyingGlass} /></li>
+			<li class="w-4 h-4 flex items-center"><Fa icon={faSliders} /></li> -->
+			{#if $appUser}
+				<li class="w-4 h-4 flex items-center"><a href="/profile"> <Fa icon={faUser} /> </a></li>
+				<li class="w-4 h-4 flex items-center">
+					<button type="button" on:click={logout}>
+						<Fa icon={faArrowRightFromBracket} />
+					</button>
+				</li>
+			{:else}
+				<li class="w-4 h-4 flex items-center"><a href="/auth/signup"> <Fa icon={faUser} /> </a></li>
+				<!-- <li><a href="/auth/login">login</a></li> -->
+			{/if}
+		</ul>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import { appUser } from '$lib/stores/appUser.js';
+	import TopNav from '$lib/components/navs/TopNav.svelte';
 
 	export let data;
 	appUser.setUser(data.user);
-
-	async function logout() {
-		await fetch('/api/logout', {
-			method: 'POST'
-		});
-		appUser.setUser(null);
-	}
 </script>
 
 {#if $appUser !== undefined}
 	<div class="flex flex-col" style="min-height: 100vh">
-		<ul>
-			<li><a href="/">home</a></li>
-			{#if $appUser}
-				<li><a href="/profile">profile</a></li>
-				<li>
-					<button type="button" on:click={logout}>logout</button>
-				</li>
-			{:else}
-				<li><a href="/auth/signup">signup</a></li>
-				<li><a href="/auth/login">login</a></li>
-			{/if}
-		</ul>
+		<TopNav />
 		<div class="flex flex-grow">
 			<slot />
 		</div>
