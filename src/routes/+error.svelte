0f1fd2b8555f0bf4df0f5f53ad19893f3a0eb02e
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-23 10:50:58 +0200

    add simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="container">
+	<div class="flex flex-col justify-center items-center">
+		{#if $page.error}
+			<h1 class="text-6xl">{$page.status}</h1>
+			<p>{$page.error.message}</p>
+		{/if}
+	</div>
+</div>
