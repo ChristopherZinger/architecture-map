eece84cda7640cf3639b4f07845444a373fc328d
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-24 12:19:04 +0200

    create SearchDropdown and Dropdown components

---
+++ src/lib/components/inputs/Dropdown.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Select from 'svelte-select';
+	import Fa from 'svelte-fa';
+	import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+	export let items: { label: string; value: string }[];
+	export let value: string;
+	export let placeholder = 'Please select';
+	export let searchable = false;
+	export let name = 'select';
+</script>
+
+<div class="border-b-2 border-b-black pb-2">
+	<Select
+		{items}
+		bind:value
+		{placeholder}
+		{name}
+		{searchable}
+		--border="none"
+		--border-focused="none"
+		--border-hover="none"
+		--padding="0.5rem"
+		--value-container-padding="0"
+	>
+		<div class:hidden={!searchable} slot="prepend" style="margin-right: 10px;">
+			<Fa icon={faMagnifyingGlass} />
+		</div>
+	</Select>
+</div>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
---
+++ src/lib/components/inputs/SearchDropdown.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Dropdown from './Dropdown.svelte';
+
+	export let items: { label: string; value: string }[];
+	export let value: string;
+	export let placeholder = 'Please select';
+	export let name = 'select';
+</script>
+
+<Dropdown bind:value searchable={true} {placeholder} {name} {items} />
