37283b2de557e675c43b286c672cb4afacbcc880
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-06-26 08:27:48 +0200

    add MarkerInCenter component

---
+++ src/lib/components/map/MarkerInCenter.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { getMap } from '$lib/utils/get-map-utils';
+	import Marker from '../leafletComponents/Marker.svelte';
+	import Pin from '../svgIcons/Pin.svelte';
+
+	export let height: string = '30px';
+
+	const map = getMap();
+
+	let position = map.getCenter();
+	map.on('dragend', () => {
+		position = map.getCenter();
+	});
+	map.on('zoomend', () => {
+		position = map.getCenter();
+	});
+
+	export { position };
+</script>
+
+<Marker latLng={[position.lat, position.lng]}>
+	<div slot="icon">
+		<Pin {height} />
+	</div>
+</Marker>
---
+++ src/lib/components/svgIcons/Pin.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let height = '18px';
+	export let color = '#CE8989';
+</script>
+
+<svg {height} viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M7 0C3.13401 0 0 3.13401 0 7C0 8.95869 0.804467 10.7295 2.10102 12H2L7 17L12 12H11.899C13.1955 10.7295 14 8.95869 14 7C14 3.13401 10.866 0 7 0Z"
+		fill={color}
+	/>
+</svg>
