d29ca62bcfb91b66693bb80a3babc52b8fa84e22
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-07-14 16:02:00 +0200

    add project styles form and modal

---
+++ src/lib/components/CreateProjectStyleModal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Button from './Button.svelte';
+	import CreateProjectStyleForm from './CreateProjectStyleForm.svelte';
+	import Modal from './Modal.svelte';
+
+	export let isOpen: boolean;
+	export let onClose: () => void;
+
+	function _onClose() {
+		error = undefined;
+		success = false;
+		onClose();
+	}
+
+	let success: boolean = false;
+	let error: string | undefined;
+</script>
+
+<Modal {isOpen} title="Create Project Style" onClose={_onClose}>
+	<div class="flex flex-col gap-10">
+		<div>
+			{#if !success}
+				<CreateProjectStyleForm bind:success bind:error />
+			{:else}
+				<div class="flex flex-col gap-5 items-center">
+					<Button onClick={_onClose} type="button">Success!</Button>
+				</div>
+			{/if}
+		</div>
+	</div>
+</Modal>
