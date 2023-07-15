150ef465e2a0a521dbeeb1a009056b82617d3bdd
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-07-15 13:27:18 +0200

    add form prop to Button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let type: 'button' | 'submit' | null | 'reset' = 'submit';
 	export let onClick: (() => void) | undefined = undefined;
+	export let form: string | undefined = undefined;
 
 	function _handler() {
 		if (onClick) {
@@ -12,5 +13,8 @@
 <button
 	on:click={_handler}
 	class="border-2 border-black h-10 hover:text-white hover:bg-black font-bold px-5"
-	{type}><slot /></button
+	{form}
+	{type}
 >
+	<slot />
+</button>
