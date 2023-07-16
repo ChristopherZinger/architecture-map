845377df200b2e8ae1b1ad1a9cec3fbf4b7bb5fc
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-07-16 13:07:26 +0200

    create MultiselectTagList component

---
+++ src/lib/components/createProject/MultiselectTagList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let selectedItems: { label: string; value: string | number }[];
+	export let onClick: (item: string | number) => void;
+	export let name: string;
+</script>
+
+{#if selectedItems.length > 0}
+	<div class="col-start-2 col-span-3 flex flex-wrap gap-5 my-5">
+		{#each selectedItems as item}
+			<button
+				type="button"
+				on:click={() => {
+					onClick(item.value);
+				}}>{item.label}</button
+			>
+			<input type="text" value={item.value} {name} hidden />
+		{/each}
+	</div>
+{/if}
