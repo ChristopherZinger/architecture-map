a2dc02287accaf44367a514bfabd628e8a6c3e05
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-07-16 13:07:00 +0200

    refine MapLngLatInput component

--- src/lib/components/map/MapLngLatInput.svelte
+++ src/lib/components/map/MapLngLatInput.svelte
@@ -17,66 +17,66 @@
 	let position: [number, number] = [lat, lng];
 </script>
 
-<div style="max-width: 800px; margin:auto">
-	<div style="height: 500px">
-		<Map
-			onMapReady={(_map) => {
-				_map.setView([lat, lng], 7); // removeing this caluses map init error
-				map = _map;
+<div style="height: 500px">
+	<Map
+		onMapReady={(_map) => {
+			_map.setView([lat, lng], 7); // removeing this caluses map init error
+			map = _map;
+		}}
+	>
+		<Tileset
+			url={`https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png`}
+			attribution={`Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
+			maxZoom={20}
+		/>
+
+		<MarkerInCenter
+			bind:markerLatLng={position}
+			onPositionChange={(p) => {
+				lat = p[0];
+				lng = p[1];
 			}}
-		>
-			<Tileset
-				url={`https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png`}
-				attribution={`Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
-				maxZoom={20}
-			/>
-
-			<MarkerInCenter
-				bind:markerLatLng={position}
-				onPositionChange={(p) => {
-					lat = p[0];
-					lng = p[1];
-				}}
-			/>
-		</Map>
+		/>
+	</Map>
+</div>
+<div class="flex gap-10 my-4">
+	<div class="flex gap-2 items-center grow">
+		<label for="lat">Lat:</label>
+		<input
+			class="grow px-4 py-2 border-b-2 border-b-black outline-none"
+			type="number"
+			placeholder="latitude"
+			name="lat"
+			value={lat}
+			on:input={({ currentTarget: { value } }) => {
+				const v = parseFloat(value);
+				if (isFinite(v)) {
+					lat = v;
+				}
+			}}
+			step="0.000000000000001"
+			min="-90"
+			max="90"
+		/>
 	</div>
-	<div class="flex gap-10 my-4">
-		<div class="flex gap-2 items-center grow">
-			<label for="lat">Lat:</label>
-			<input
-				class="grow px-4 py-2 border-b-2 border-b-black outline-none"
-				type="number"
-				placeholder="latitude"
-				name="lat"
-				value={lat}
-				on:input={({ currentTarget: { value } }) => {
-					const v = parseFloat(value);
-					if (isFinite(v)) {
-						lat = v;
-					}
-				}}
-				min={-90}
-				max={90}
-			/>
-		</div>
 
-		<div class="flex gap-2 items-center grow">
-			<label for="lng">Lng:</label>
-			<input
-				class="grow px-4 py-2 border-b-2 border-b-black outline-none"
-				type="number"
-				name="lng"
-				placeholder="longitude"
-				value={lng}
-				on:input={({ currentTarget: { value } }) => {
-					const v = parseFloat(value);
-					if (isFinite(v)) {
-						lng = v;
-					}
-				}}
-				min={-180}
-				max={180}
-			/>
-		</div>
+	<div class="flex gap-2 items-center grow">
+		<label for="lng">Lng:</label>
+		<input
+			class="grow px-4 py-2 border-b-2 border-b-black outline-none"
+			type="number"
+			name="lng"
+			placeholder="longitude"
+			value={lng}
+			on:input={({ currentTarget: { value } }) => {
+				const v = parseFloat(value);
+				if (isFinite(v)) {
+					lng = v;
+				}
+			}}
+			step="0.000000000000001"
+			min="-180"
+			max="180"
+		/>
 	</div>
 </div>
