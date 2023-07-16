0ba080b14c9e671ab59f81c42694878fc3e4f388
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-07-16 13:03:55 +0200

    create number input component

---
+++ src/lib/components/inputs/NumberInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let name: string;
+	export let min: number | undefined = undefined;
+	export let max: number | undefined = undefined;
+	export let step: number = 1;
+	export let placeholder: string | undefined = undefined;
+</script>
+
+<input
+	type="number"
+	{placeholder}
+	{step}
+	{name}
+	{min}
+	{max}
+	class="border-b-2 border-b-black w-full p-4"
+/>
